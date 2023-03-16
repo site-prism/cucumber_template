92609741e31bfdb2cc96660479d73f57ca13d718
Luke Hill <lukehill_uk@hotmail.com>
2023-03-16 13:49:30 +0000

    Add in sections/pages placeholder

---
+++ features/support/pages/all.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Add in all of the individual pages you want here
+#
+# Mine are omitted as they were company specific. Add in whichever page/s you need
---
+++ features/support/sections/all.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Add in all of the individual sections you want here
+#
+# Mine are omitted as they were company specific. Add in whichever section/s you need
