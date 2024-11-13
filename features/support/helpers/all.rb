5a6897495d1836554d579a2ba85be6ba8fe74646
Luke Hill <lukehill_uk@hotmail.com>
2024-11-13 15:26:36 +0000

    Add more sample helper logic

--- features/support/helpers/all.rb
+++ features/support/helpers/all.rb
@@ -3,6 +3,10 @@
 # Add in all of the helpers you might need here. Again have this mirror your folder
 
 require_relative 'env_variables'
+
 require_relative 'methods'
+
+require_relative 'data'
+require_relative 'javascript'
 require_relative 'page'
 require_relative 'regex'
--- features/support/helpers/page.rb
+++ features/support/helpers/page.rb
@@ -2,6 +2,56 @@
 
 module Helpers
   module Page
-    # Add in helpers here relevant to the physical page construct - such as resizing a window
+    include Helpers::Methods
+    include Helpers::EnvVariables
+    include Helpers::Javascript
+
+    def resize_window(desired_width = width, desired_height = height)
+      AutomationLogger.warn("Resizing Windows on Browserstack IE can be flaky!") if browserstack? && internet_explorer?
+      Capybara.current_window.resize_to(desired_width, desired_height)
+    end
+
+    def save_window_screenshot(scenario)
+      path = image_file_path(scenario)
+      AutomationLogger.info("Taking a screenshot to #{path}")
+      current_session.save_screenshot(path)
+      attach(path, "image/png")
+    end
+
+    def send_tabs(count)
+      count.times do
+        if safari?
+          send_keys(%i[alt tab])
+          # In Safari the modifier key (OPTION, but called :alt), is not
+          # released after being used in the alt+tab call. This means that
+          # subsequent calls do things like alt+click which downloads a link.
+          #
+          # See: https://bugs.webkit.org/show_bug.cgi?id=219948
+          # LH - Dec 2020
+          send_keys(:alt)
+        else
+          send_keys(:tab)
+        end
+        sleep 0.1
+      end
+    end
+
+    private
+
+    def image_file_path(test_case)
+      "#{ENV['BASE_ARTIFACTS_PATH']}/screenshots/#{name_of_file(test_case)}.png"
+    end
+
+    def name_of_file(test_case)
+      "#{timestamp}_#{test_case.feature_file_name}_#{test_case.name.gsub('/', 'or').gsub(' ', '_')}"
+    end
+
+    def width(fallback: 1600)
+      ENV.fetch("BROWSER_WIDTH", fallback)
+    end
+
+    def height(fallback: 800)
+      ENV.fetch("BROWSER_HEIGHT", fallback)
+    end
   end
 end
