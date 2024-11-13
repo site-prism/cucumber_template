86e669af3c7944d0615e2423a716317642419029
Luke Hill <lukehill_uk@hotmail.com>
2024-11-13 15:01:26 +0000

    Remove all outdated hints and add basic env vars to permit running of a sample feature

--- features/support/helpers/env_variables.rb
+++ features/support/helpers/env_variables.rb
@@ -3,5 +3,25 @@
 module Helpers
   module EnvVariables
     # Add in helpers here relevant to the env construct - such as boolean checks on browser types
+
+    def base_url
+      'https://www.google.com/'
+    end
+
+    def browser
+      :chrome
+    end
+
+    def browserstack?
+      false
+    end
+
+    def safari?
+      false
+    end
+
+    def log_level
+      :INFO
+    end
   end
 end
